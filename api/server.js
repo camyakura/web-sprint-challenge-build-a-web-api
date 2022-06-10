4d2a9b1845acfdf536c2c952b23a6cd354076ff8
TakeoYakura <92492416+TakeoYakura@users.noreply.github.com>
2022-06-10 10:54:55 -0700

    setup actions router

--- api/server.js
+++ api/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const server = express();
 const projectRouter = require('./projects/projects-router')
+const actionRouter = require('./actions/actions-router')
 
 server.use(express.json())
 server.use('/api/projects', projectRouter)
+server.use('/api/actions', actionRouter)
 
 module.exports = server;
 
