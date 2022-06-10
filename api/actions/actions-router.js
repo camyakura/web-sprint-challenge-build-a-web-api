496cc7d36357ca47d6811e71042112f067ed26bf
TakeoYakura <92492416+TakeoYakura@users.noreply.github.com>
2022-06-10 10:59:57 -0700

    first GET actions endpoint working

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,5 +1,18 @@
 const express = require('express')
+const {
+    validateAction,
+    validateActionId,
+} = require('./actions-middlware')
 const Action = require('./actions-model')
 const router = express.Router()
+
+router.get('/', async (req, res, next) => {
+    try {
+        const result = await Action.get()
+        res.json(result)
+    } catch(err) {
+        next(err)
+    }
+})
 
 module.exports = router
