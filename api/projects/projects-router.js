5c72095cb8c18d20e2652847f84aba7c6e650e2d
TakeoYakura <92492416+TakeoYakura@users.noreply.github.com>
2022-06-10 10:28:42 -0700

    first projects get endpoint created

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,5 +1,16 @@
 const express = require('express')
-
+const Project = require('./projects-model')
 const router = express.Router()
+
+router.get('/', async (req, res, next) => {
+    try {
+        const result = await Project.get()
+        res.json(result)
+    } catch(err){
+        next(err)
+    }
+})
+
+
 
 module.exports = router
