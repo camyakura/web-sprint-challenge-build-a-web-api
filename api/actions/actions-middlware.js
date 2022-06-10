86bd6ab58d26490a20f2b73e4d144a9797a76a22
TakeoYakura <92492416+TakeoYakura@users.noreply.github.com>
2022-06-10 10:58:08 -0700

    created actions middleware

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,30 @@
-// add middlewares here related to actions
+const Action = require('./actions-model')
+
+async function validateActionId (req, res, next) {
+    try {
+        const action = await Action.get(req.params.id)
+        if(action) {
+            next()
+        } else {
+            res.status(404).json({message: 'Action not found'})
+        }
+    } catch(err) {
+        next(err)
+    }
+}
+
+async function validateAction (req, res, next) {
+    const {project_id, description, notes, completed} = req.body
+    try{
+        if(!project_id || !description || !notes || typeof completed === 'undefined') {
+            next({status: 400, message: 'Name and Description required'})
+        } else {
+            req.valid = {project_id, description, notes, completed}
+            next()
+        }
+    } catch(err) {
+        next(err)
+    }
+}
+
+module.exports = {validateAction, validateActionId}
